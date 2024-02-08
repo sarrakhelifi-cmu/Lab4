42ec2ba9702089938790e6b735f56e50224ccc20
sarra khelifi <skhelifi@andrew.cmu.edu>
2024-02-08 11:09:10 +0300

    Migrated

---
+++ db/migrate/20240208080800_create_book_authors.rb
@@ -0,0 +1,10 @@
+class CreateBookAuthors < ActiveRecord::Migration[7.0]
+  def change
+    create_table :book_authors do |t|
+      t.integer :book_id
+      t.integer :author_id
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/factories/book_authors.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :book_author do
+    book_id 1
+    author_id 1
+  end
+end
