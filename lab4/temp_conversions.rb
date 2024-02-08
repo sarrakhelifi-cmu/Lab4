8b7edfc8dfa25d267bb4788662f33d5eacc3aa6e
sarra khelifi <skhelifi@andrew.cmu.edu>
2024-02-08 12:25:29 +0300

    added file

--- lab4/temp_conversions.rb
+++ lab4/temp_conversions.rb
@@ -1,7 +1,11 @@
 def convert(temp, measure="F")
-    return (temp-32)*5/9
+    return "Temperature must be an integer" if temp.class != Integer
+    return "Temperature below Absolute Zero" if temp < -474 
+    ((temp-32)*5)/9 
 end 
 
 puts convert(32)          
 puts convert(50)          
 puts convert(212)
+puts convert("zero")
+puts convert(-500)
