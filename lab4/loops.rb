c6e84396b4890438d953cc1b175a2df7e0daa6b0
sarra khelifi <skhelifi@andrew.cmu.edu>
2024-02-10 16:27:33 +0300

    added

---
+++ lab4/loops.rb
@@ -0,0 +1,15 @@
+ 
+    array = [10, 91, 2, 333, 4, 5]
+    for elem in array 
+        puts elem
+    end 
+
+    array2 = ["hi", "you", "ruby", "programming"]
+    for elem in array2 
+        puts elem
+    end 
+
+    for i in 1..5
+        puts "value of local variable is #{i}"
+        puts array [i] 
+    end
--- lab4/temp_conversions.rb
+++ lab4/temp_conversions.rb
@@ -1,3 +1,13 @@
+def Zero? (tmp, measure)
+    if measure == "F"
+       return true if tmp< -460  
+    elsif measure == "C"
+       return true if tmp < -273 
+    else
+        return false
+    end
+end
+
 def convert(temp, measure="F")
     return "Temperature must be an integer" if temp.class != Integer
     return "Temperature below Absolute Zero" if temp < -474 
@@ -9,3 +19,7 @@ puts convert(50)
 puts convert(212)
 puts convert("zero")
 puts convert(-500)
+puts convert(0, "C")
+puts convert(10, "C")
+puts convert(100, "C")
+puts convert(-200, "C")
